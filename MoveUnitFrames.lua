bb4b896e6cda26f8c25e01ea976b03585de4c32b
Martin Hagström <marhag87@gmail.com>
2016-07-21 12:33:16 +0200

    Initial commit

---
+++ MoveUnitFrames.lua
@@ -0,0 +1,2 @@
+PlayerFrame:ClearAllPoints()PlayerFrame:SetPoint("CENTER",UIParent,"CENTER",-160,-200)PlayerFrame.SetPoint=function()end
+TargetFrame:ClearAllPoints()TargetFrame:SetPoint("CENTER",UIParent,"CENTER",160,-200)TargetFrame.SetPoint=function()end
