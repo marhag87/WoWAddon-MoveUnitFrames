9742f122f979a48b31c50764209641cc69a8d8ff
Martin Hagström <marhag87@gmail.com>
2016-08-21 17:13:22 +0200

    Don't taint unit frames
    Thanks to http://www.wowinterface.com/forums/showthread.php?t=53794

--- MoveUnitFrames.lua
+++ MoveUnitFrames.lua
@@ -1,2 +1,26 @@
-PlayerFrame:ClearAllPoints()PlayerFrame:SetPoint("CENTER",UIParent,"CENTER",-160,-200)PlayerFrame.SetPoint=function()end
-TargetFrame:ClearAllPoints()TargetFrame:SetPoint("CENTER",UIParent,"CENTER",160,-200)TargetFrame.SetPoint=function()end
+local MoveUnitFrames = CreateFrame("FRAME", "MoveUnitFramesFrame");
+MoveUnitFrames:RegisterEvent("VARIABLES_LOADED");
+MoveUnitFrames:RegisterEvent("PLAYER_LOGOUT");
+
+local PlayerX = -160
+local PlayerY = -200
+local TargetX = 160
+local TargetY = -200
+
+function MoveUnitFrames:OnEvent(event, arg1)
+    if event == "VARIABLES_LOADED" then
+        PlayerFrame:ClearAllPoints();
+        PlayerFrame:SetPoint("CENTER", UIParent, "CENTER", PlayerX, PlayerY);
+        PlayerFrame:SetUserPlaced(true);
+ 
+        TargetFrame:ClearAllPoints();
+        TargetFrame:SetPoint("CENTER", UIParent, "CENTER", TargetX, TargetY);
+        TargetFrame:SetUserPlaced(true);
+
+    elseif event == "PLAYER_LOGOUT" then
+        PlayerFrame:SetUserPlaced(false);
+        TargetFrame:SetUserPlaced(false);
+    end
+end
+
+MoveUnitFrames:SetScript("OnEvent", MoveUnitFrames.OnEvent);
