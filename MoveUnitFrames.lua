4edaf2034ee13fbb56d041a399bfbb4815930125
Martin Hagström <marhag87@gmail.com>
2016-08-21 17:14:45 +0200

    Manage focus frame

--- MoveUnitFrames.lua
+++ MoveUnitFrames.lua
@@ -6,6 +6,8 @@ local PlayerX = -160
 local PlayerY = -200
 local TargetX = 160
 local TargetY = -200
+local FocusX = 400
+local FocusY = -200
 
 function MoveUnitFrames:OnEvent(event, arg1)
     if event == "VARIABLES_LOADED" then
@@ -17,9 +19,14 @@ function MoveUnitFrames:OnEvent(event, arg1)
         TargetFrame:SetPoint("CENTER", UIParent, "CENTER", TargetX, TargetY);
         TargetFrame:SetUserPlaced(true);
 
+        FocusFrame:ClearAllPoints();
+        FocusFrame:SetPoint("CENTER", UIParent, "CENTER", FocusX, FocusY);
+        FocusFrame:SetUserPlaced(true);
+
     elseif event == "PLAYER_LOGOUT" then
         PlayerFrame:SetUserPlaced(false);
         TargetFrame:SetUserPlaced(false);
+        FocusFrame:SetUserPlaced(false);
     end
 end
 
